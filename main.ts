5df94025f09bb16489d559a40b4afad6684041db
kaise-ksrobot <arthur@kaise.com.tw>
2020-01-03 20:15:43 +0800

    v1.4.0

--- main.ts
+++ main.ts
@@ -13,6 +13,7 @@ namespace KSRobot_IOT {
     let iot_receive_data = ""
     let receive_topic_name = ""
     let receive_topic_value = ""
+    let Version = "1"
 
     let MQTT_TOPIC = ["", "", "", "", ""]
     let MQTT_CB: Action[] = [null, null, null, null, null]
@@ -51,6 +52,7 @@ namespace KSRobot_IOT {
             let compare_str1 = "@#$@TOPIC$"
             let compare_str2 = "@#$@STIP$"
             let compare_str3 = "@#$@APIP$"
+            let compare_str4 = "@#$@Version$"
             let strlen4 = 0
             let strlen3 = 0
             let strlen2 = 0
@@ -79,7 +81,7 @@ namespace KSRobot_IOT {
 
             }
             // parse Local IP
-            if (iot_receive_data.indexOf(compare_str2) >= 0) {
+            else if (iot_receive_data.indexOf(compare_str2) >= 0) {
                 strlen1 = iot_receive_data.indexOf(compare_str2) + compare_str2.length
                 strlen2 = iot_receive_data.indexOf(compare_str0) - strlen2
                 strlen3 = iot_receive_data.indexOf(compare_str0) + compare_str0.length
@@ -96,13 +98,21 @@ namespace KSRobot_IOT {
                 `)
             }
             // parse AP information
-            if (iot_receive_data.indexOf(compare_str3) >= 0) {
+            else if (iot_receive_data.indexOf(compare_str3) >= 0) {
                 strlen1 = iot_receive_data.indexOf(compare_str3) + compare_str3.length
                 strlen2 = iot_receive_data.indexOf(compare_str0) - strlen3
                 strlen3 = iot_receive_data.indexOf(compare_str0) + compare_str0.length
                 strlen4 = iot_receive_data.length - strlen3
                 temp_name = iot_receive_data.substr(strlen1, strlen2)
                 ap_ip = iot_receive_data.substr(strlen3, strlen4)
+            }
+            else if (iot_receive_data.indexOf(compare_str4) >= 0) {
+                strlen1 = iot_receive_data.indexOf(compare_str4) + compare_str4.length
+                strlen2 = iot_receive_data.indexOf(compare_str0) - strlen3
+                strlen3 = iot_receive_data.indexOf(compare_str0) + compare_str0.length
+                strlen4 = iot_receive_data.length - strlen3
+                temp_name = iot_receive_data.substr(strlen1, strlen2)
+                Version = iot_receive_data.substr(strlen3, strlen4)
             }
 
 
@@ -129,10 +139,11 @@ namespace KSRobot_IOT {
         );
         serial.setRxBufferSize(128)
         serial.setTxBufferSize(128)
+        control.waitMicros(500000)
         WifiDataReceived()
-        control.waitMicros(500000)
+        control.waitMicros(200000)
         serial.writeLine("AT+Restart=");
-        control.waitMicros(500000)
+        control.waitMicros(1300000)
         serial.writeLine("AT+AP_SET?ssid=" + ssid + "&pwd=" + passwd + "&AP=" + ap + "=");
         for (let id_y = 0; id_y <= 4; id_y++) {
             for (let id_x = 0; id_x <= 4; id_x++) {
@@ -144,6 +155,7 @@ namespace KSRobot_IOT {
 
             }
         }
+        
 
     }
 
@@ -156,6 +168,12 @@ namespace KSRobot_IOT {
     //% block="IOT AP Name"
     export function AP_Name(): string {
         return ap_ip;
+    }
+
+    //% blockId=Get_Version
+    //% block="Get Version"
+    export function Get_Version(): string {
+        return Version;
     }
 
     //% blockId=Wifi_Connection
@@ -215,8 +233,8 @@ namespace KSRobot_IOT {
     export function MQTT_set(host: string, port: number, clientId: string, username: string, pwd: string): void {
         if (IOT_WIFI_CONNECTED) {
             serial.writeLine("AT+MQTT?host=" + host + "&port=" + port + "&clientId=" + clientId + "&username=" + username + "&password=" + pwd + "=");
+            control.waitMicros(1000000)
             IOT_MQTT_CONNECTED = true
-            control.waitMicros(800000)
         }
     }
 
@@ -224,7 +242,7 @@ namespace KSRobot_IOT {
     //% block="MQTT publish topic %topic payload %payload"
     export function MQTTPublish(topic: string, payload: string): void {
         if (IOT_MQTT_CONNECTED) {
-            serial.writeLine("AT+MQTT_Publish?topic=" + topic + "&payload=" + payload + "=");
+            serial.writeLine("AT+MQTT_Publish?topic=" + topic + "&payload=" + payload );
         }
     }
 
@@ -232,9 +250,9 @@ namespace KSRobot_IOT {
     //% block="MQTT subscribe topic %topic"
     export function MQTTSubscribe(topic: string): void {
         if (IOT_MQTT_CONNECTED) {
-            
+            control.waitMicros(600000)
             serial.writeLine("AT+MQTT_Subscribe?topic=" + topic + "=");
-            control.waitMicros(800000)
+            
         }
     }
 
@@ -256,11 +274,11 @@ namespace KSRobot_IOT {
     export function MQTTPublish1(top: TOPIC, payload: string): void {
         if (IOT_MQTT_CONNECTED) {
             switch (top) {
-                case TOPIC.Topic0: serial.writeLine("AT+MQTT_Publish?topic=" + MQTT_TOPIC[0] + "&payload=" + payload + "="); break;
-                case TOPIC.Topic1: serial.writeLine("AT+MQTT_Publish?topic=" + MQTT_TOPIC[1] + "&payload=" + payload + "="); break;
-                case TOPIC.Topic2: serial.writeLine("AT+MQTT_Publish?topic=" + MQTT_TOPIC[2] + "&payload=" + payload + "="); break;
-                case TOPIC.Topic3: serial.writeLine("AT+MQTT_Publish?topic=" + MQTT_TOPIC[3] + "&payload=" + payload + "="); break;
-                case TOPIC.Topic4: serial.writeLine("AT+MQTT_Publish?topic=" + MQTT_TOPIC[4] + "&payload=" + payload + "="); break;
+                case TOPIC.Topic0: serial.writeLine("AT+MQTT_Publish?topic=" + MQTT_TOPIC[0] + "&payload=" + payload ); break;
+                case TOPIC.Topic1: serial.writeLine("AT+MQTT_Publish?topic=" + MQTT_TOPIC[1] + "&payload=" + payload ); break;
+                case TOPIC.Topic2: serial.writeLine("AT+MQTT_Publish?topic=" + MQTT_TOPIC[2] + "&payload=" + payload ); break;
+                case TOPIC.Topic3: serial.writeLine("AT+MQTT_Publish?topic=" + MQTT_TOPIC[3] + "&payload=" + payload ); break;
+                case TOPIC.Topic4: serial.writeLine("AT+MQTT_Publish?topic=" + MQTT_TOPIC[4] + "&payload=" + payload ); break;
             }
         }
     }
@@ -269,10 +287,10 @@ namespace KSRobot_IOT {
     //% block="MQTT subscribe  %top | %topic"
     export function MQTTSubscribe1(top: TOPIC, topic: string): void {
         if (IOT_MQTT_CONNECTED) {
-            
+            control.waitMicros(600000)
             MQTT_TOPIC[top] = topic
             serial.writeLine("AT+MQTT_Subscribe?topic=" + topic + "=");
-            control.waitMicros(800000)
+            
             
         }
 
